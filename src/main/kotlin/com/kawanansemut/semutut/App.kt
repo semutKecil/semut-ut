ff90b0131a3ab3df209d6e36a958771f0f58cd23
herlanS_ <n0n1sakuhana>
2019-09-20 10:29:53 +0700

    initial commit

---
+++ src/main/kotlin/com/kawanansemut/semutut/App.kt
@@ -0,0 +1,8 @@
+package com.kawanansemut.semutut
+
+class App
+
+fun main(args: Array<String>) {
+    println("ok")
+}
+
---
+++ src/main/kotlin/com/kawanansemut/semutut/entity/Base.kt
@@ -0,0 +1,41 @@
+package com.kawanansemut.semutut.entity
+
+import java.io.Serializable
+import java.time.LocalDateTime
+import java.util.*
+import com.fasterxml.jackson.annotation.JsonBackReference
+import org.hibernate.annotations.NaturalId
+import javax.persistence.*
+
+
+@MappedSuperclass
+open class Base : Serializable {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @JsonBackReference
+    val id: Long = 0L
+
+    @NaturalId
+    @Column(unique = true)
+    var uuid: String = UUID.randomUUID().toString().replace("-", "")
+
+    @JsonBackReference
+    var deleted: Boolean = false
+
+    @Basic
+    var updated: LocalDateTime = LocalDateTime.now()
+
+    @Basic
+    var created: LocalDateTime = LocalDateTime.now()
+
+
+    @PreUpdate
+    internal fun onPreUpdate() {
+        updated = LocalDateTime.now()
+    }
+
+    @PrePersist
+    internal fun onPrePersist() {
+        updated = created
+    }
+}
